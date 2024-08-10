39659d6abb41da3e7fe71245cc8b719fc42532ff
rakshapadiyar <rakshapadiyar100@gmail.com>
2024-08-10 15:42:27 +0530

    ep 3 done

---
+++ app.js
@@ -0,0 +1,10 @@
+var a=10;
+var b=20;
+console.log("Namaste Node.js")
+console.log(a+b);
+
+console.log(global);
+console.log(this); // {}
+console.log(globalThis); //same global obj
+
+console.log(global === globalThis) //true
